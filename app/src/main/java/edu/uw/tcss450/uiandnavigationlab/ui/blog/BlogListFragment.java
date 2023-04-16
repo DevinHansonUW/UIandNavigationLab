32982b713b1131e1d649a25a2365794141dea8d8
Devin  Hanson <devingh2@uw.edu>
2023-04-15 22:28:48 -0700

    Finished implementation of Lab 3.

--- app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/blog/BlogListFragment.java
+++ app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/blog/BlogListFragment.java
@@ -2,7 +2,10 @@ package edu.uw.tcss450.uiandnavigationlab.ui.blog;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.uiandnavigationlab.R;
+import edu.uw.tcss450.uiandnavigationlab.databinding.FragmentBlogListBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +23,8 @@ import edu.uw.tcss450.uiandnavigationlab.R;
  * create an instance of this fragment.
  */
 public class BlogListFragment extends Fragment {
+
+    private BlogListViewModel mModel;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -54,32 +60,32 @@ public class BlogListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mModel = new ViewModelProvider(getActivity()).get(BlogListViewModel.class);
+        mModel.connectGet();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_blog_list, container, false);
-        if (view instanceof RecyclerView) {
-            //Try out a grid layout to achieve rows AND columns. Adjust the widths of the
-            //cards on display
-            ((RecyclerView) view).setLayoutManager(new GridLayoutManager(getContext(), 1));
-
-            //Try out horizontal scrolling. Adjust the widths of the card so that it is
-            //obvious that there are more cards in either direction. i.e. don't have the cards
-            //span the entire witch of the screen. Also, when considering horizontal scroll
-            //on recycler view, ensure that thre is other content to fill the screen.
-//           ((LinearLayoutManager)((RecyclerView) view).getLayoutManager())
-//                    .setOrientation(LinearLayoutManager.HORIZONTAL);
-
-            ((RecyclerView) view).setAdapter(
-                    new BlogRecyclerViewAdapter(BlogGenerator.getBlogList()));
-        }
-        return view;
+        return inflater.inflate(R.layout.fragment_blog_list, container, false);
 
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        FragmentBlogListBinding binding = FragmentBlogListBinding.bind(getView());
+
+        mModel.addBlogListObserver(getViewLifecycleOwner(), blogList -> {
+            if (!blogList.isEmpty()) {
+                binding.listRoot.setAdapter(
+                        new BlogRecyclerViewAdapter(blogList)
+                );
+                binding.layoutWait.setVisibility(View.GONE);
+            }
+        });
+    }
+
 }
